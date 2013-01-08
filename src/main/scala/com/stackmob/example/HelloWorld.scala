382322f23c289dacb6c176b2ebad411fa8c02324
Justin <justin@stackmob.com>
2013-01-08 12:00:20 -0800

    Update src/main/scala/com/stackmob/example/HelloWorld.scala

--- src/main/scala/com/stackmob/example/HelloWorld.scala
+++ src/main/scala/com/stackmob/example/HelloWorld.scala
@@ -25,7 +25,7 @@ import scala.collection.JavaConverters._
 
 class HelloWorld extends CustomCodeMethod {
 
-  override def getMethodName: String = "helloworld"
+  override def getMethodName: String = "helloWorld"
 
   override def getParams: JList[String] = List[String]().asJava
 
